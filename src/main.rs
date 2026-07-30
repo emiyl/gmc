c182fadedced46a94c2b04547c8a4fe569cdbeea
emiyl <me@emiyl.com>
2026-07-30 13:35:08 +0100

    add some basic tests

--- src/main.rs
+++ src/main.rs
@@ -53,6 +53,19 @@ impl Program {
     }
 }
 
+fn create_program_from_gml(input: &str) -> Program {
+    let lexer = Lexer::new(input.to_string());
+    let mut parser = Parser::new(lexer);
+    let program_ast = parser.parse_program();
+    log::debug!("Parsed AST: {:#?}", program_ast);
+
+    let mut compiler = Compiler::new();
+    compiler.compile_program(&program_ast);
+
+    let mut resolver = Resolver::new();
+    resolver.resolve(compiler.instructions)
+}
+
 fn main() {
     let args: Args = ClapParser::parse();
     Builder::new()
@@ -61,18 +74,7 @@ fn main() {
         .init();
 
     let input = std::fs::read_to_string(&args.input).expect("Failed to read input file");
-
-    let lexer = Lexer::new(input);
-    let mut parser = Parser::new(lexer);
-
-    let program = parser.parse_program();
-    log::debug!("Parsed AST: {:#?}", program);
-
-    let mut compiler = Compiler::new();
-    compiler.compile_program(&program);
-
-    let mut resolver = Resolver::new();
-    let program = resolver.resolve(compiler.instructions);
+    let program = create_program_from_gml(&input);
 
     print_disassembly(&program.bytecode);
 
@@ -89,5 +91,61 @@ fn main() {
             data_win::build_data_win(&args.input, &program.bytecode.data, &variable_names);
         let output_file = args.output.as_ref().unwrap();
         std::fs::write(output_file, output_data).expect("Failed to write output file");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestCase {
+        input: &'static str,
+        expected_bytecode: &'static [u32],
+    }
+
+    const TEST_CASES: &[TestCase] = &[
+        TestCase {
+            input: "a = 1;",
+            expected_bytecode: &[
+                0x840F0001, // PushI.e 1
+                0x4525FFFA, // Pop.v.i
+                0xA0000000, // Variable index 0
+            ],
+        },
+        TestCase {
+            input: "a = 1; b = 2; c = a + b;",
+            expected_bytecode: &[
+                0x840F0001, // PushI.e 1
+                0x4525FFFA, // Pop.v.i
+                0xA0000000, // Variable index 0
+                0x840F0002, // PushI.e 2
+                0x4525FFFA, // Pop.v.i
+                0xA0000001, // Variable index 1
+                0xC005FFFA, // Push.v.i
+                0xA0000000, // Variable index 0
+                0xC005FFFA, // Push.v.i
+                0xA0000001, // Variable index 1
+                0x0C550000, // Add
+                0x4525FFFA, // Pop.v.i
+                0xA0000002, // Variable index 2
+            ],
+        },
+    ];
+
+    #[test]
+    fn test() {
+        for test_case in TEST_CASES {
+            let program = create_program_from_gml(test_case.input);
+            let bytecode = &program.bytecode.data;
+            let bytecode_as_u32_chunks = bytecode
+                .chunks_exact(4)
+                .map(|chunk| {
+                    let mut array = [0u8; 4];
+                    array.copy_from_slice(chunk);
+                    u32::from_le_bytes(array)
+                })
+                .collect::<Vec<u32>>();
+            assert_eq!(bytecode_as_u32_chunks, test_case.expected_bytecode);
+        }
     }
 }
